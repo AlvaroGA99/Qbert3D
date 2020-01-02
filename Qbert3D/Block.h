95f7e6bc6a0544e4a70c16a0bb7ae3b3a64803ae
rodron <alexsp2399@gmail.com>
2020-01-02 22:38:41 +0100

    Generación del mapa de Qbert hecho

--- Qbert3D/Block.h
+++ Qbert3D/Block.h
@@ -2,5 +2,12 @@
 #include "Cube.h"
 class Block : public Cube
 {
+private:
+	Vector3D mapPosition;
+public:
+	Block() : Cube() {}
+
+	Block(Vector3D position, Color color, Vector3D speed, Vector3D orientation) :
+		Cube(position, color, speed, orientation) {		this->SetIsAffectedByGravity(false);	}
 };
 
